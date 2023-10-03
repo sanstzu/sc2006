28c587f681cdb528ba6895f72ed162bb9b5e81be
Clayton Fernalo <clafernalo@gmail.com>
2023-10-04 05:46:47 +0800

    fix: backend

--- backend/backend/index.js
+++ backend/backend/index.js
@@ -6,6 +6,6 @@ const app = express();
 app.use(express.json());
 app.use("/hello", helloRoute);
 
-app.listen(3002, () => {
+app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
