217a4c5cdd1db9b975d69acfce1e2762c27468ce
JeremyNathanJusuf <Jeremynathan0502@gmail.com>
2023-10-30 14:28:13 +0800

    fix: modified return types and added error handling

--- backend/api/src/controllers/maps/searchPlace.ts
+++ backend/api/src/controllers/maps/searchPlace.ts
@@ -26,34 +26,40 @@ type rawResult = {
 async function searchPlace(
   req: Request,
   res: Response<ResponseType<SearchPlaceType[]>>
-) { 
-  const searchInput = req.body.input;
-  const url = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?';
-  const resp = await axios.get(url, {
-    headers : {}, 
-    params: {
-      key : process.env.GOOGLE_MAPS_KEY,
-      input: searchInput,
-      region: 'SG'
-    }
-  });
-
-  let respData: SearchPlaceType[] = [];
-  let results = resp.data.predictions;
-
-  results.forEach((result: rawResult) => {
-    respData.push({
-      name: result.structured_formatting.main_text,
-      address: result.structured_formatting.secondary_text,
-      place_id: result.place_id
+) {
+  try {
+    const searchInput = req.body.input;
+    const url = "https://maps.googleapis.com/maps/api/place/autocomplete/json?";
+    const resp = await axios.get(url, {
+      headers: {},
+      params: {
+        key: process.env.GOOGLE_MAPS_KEY,
+        input: searchInput,
+        region: "SG",
+      },
     });
-  });
 
-  res.status(200).json({
-    status : 1,
-    message : "success",
-    data : respData
-  });
+    let respData: SearchPlaceType[] = [];
+    let results = resp.data.predictions;
+
+    results.forEach((result: rawResult) => {
+      respData.push({
+        name: result.structured_formatting.main_text,
+        address: result.structured_formatting.secondary_text,
+        place_id: result.place_id,
+      });
+    });
+
+    res.status(200).json({
+      status: 1,
+      message: "success",
+      data: respData,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 0,
+      message: "Internal server error",
+    });
+  }
 }
-
 export default searchPlace;
